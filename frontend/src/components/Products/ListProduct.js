5d8eeb4a05f6f67c893476c8a0a163df454210f6
Vinh Ngo <ngoquangvinh1997tg@gmail.com>
2021-10-30 00:37:38 +0700

    change nuka carousel pakage to swiper carousel pakage

--- frontend/src/components/Products/ListProduct.js
+++ frontend/src/components/Products/ListProduct.js
@@ -1,102 +1,84 @@
-import React, { useEffect, useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import SwiperCore, { Autoplay, Navigation } from "swiper";
+import "swiper/components/navigation/navigation.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/swiper.min.css";
 import { getProducts } from "../../app/productThunk";
 import Loading from "../UI/Loading";
 import Product from "./Product";
-import Carousel from "nuka-carousel";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+
+// install Swiper modules
+SwiperCore.use([Navigation, Autoplay]);
 const ListProduct = () => {
+  const navigationPrevRef = useRef(null);
+  const navigationNextRef = useRef(null);
   const dispatch = useDispatch();
   const { products, loading } = useSelector((state) => state.products);
-  const [widthScreen, setWidthScreen] = useState(
-    window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
-  );
 
-  let slidesToShow = 2;
-  if (widthScreen > 640) {
-    slidesToShow = 3;
-  }
-  if (widthScreen > 768) {
-    slidesToShow = 4;
-  }
-  if (widthScreen > 1024) {
-    slidesToShow = 5;
-  }
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWidthScreen(
-        window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
-      );
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+  const breakpoints = {
+    640: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  };
 
   useEffect(() => {
     // dispatch async action to Thunk
     dispatch(getProducts());
   }, [dispatch]);
 
-  const getControlsContainerStyles = (key) => {
-    switch (key) {
-      case "BottomCenter":
-        return {
-          bottom: -60,
-        };
-      default:
-        return {
-          backgroundColor: "blue",
-        };
-    }
-  };
-
-  const renderBottomCenterControls = ({ previousSlide, nextSlide }) => {
-    return (
-      <div className="flex items-center">
-        <div className="p-2" onClick={previousSlide}>
-          <ChevronLeftIcon className="h-7" />
-        </div>
-        <div className="p-2" onClick={nextSlide}>
-          <ChevronRightIcon className="h-7" />
-        </div>
-      </div>
-    );
-  };
-
   return (
     <section>
       <h2 className="text-2xl font-bold uppercase">BEST SALLER</h2>
       {loading && <Loading />}
       {!loading && products && (
-        <Carousel
-          autoplay
-          autoplayInterval={2000}
-          wrapAround
-          cellSpacing={20}
-          slidesToShow={slidesToShow}
-          renderCenterLeftControls={({ previousSlide }) => null}
-          renderCenterRightControls={({ nextSlide }) => null}
-          getControlsContainerStyles={getControlsContainerStyles}
-          renderBottomCenterControls={renderBottomCenterControls}
-          speed={100}
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={2}
+          onInit={(swiper) => {
+            swiper.params.navigation.prevEl = navigationPrevRef.current;
+            swiper.params.navigation.nextEl = navigationNextRef.current;
+            swiper.navigation.init();
+            swiper.navigation.update();
+          }}
+          breakpoints={breakpoints}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
         >
           {products?.map(({ _id, images, name, discount, price, rating }) => (
-            <Product
-              key={_id}
-              images={images}
-              name={name}
-              discount={discount}
-              price={price}
-              rating={rating}
-              _id={_id}
-            />
+            <SwiperSlide key={_id}>
+              <Product
+                key={_id}
+                images={images}
+                name={name}
+                discount={discount}
+                price={price}
+                rating={rating}
+                _id={_id}
+              />
+            </SwiperSlide>
           ))}
-        </Carousel>
+
+          <div className="flex justify-center items-center mt-3">
+            <div className="p-2 cursor-pointer" ref={navigationPrevRef}>
+              <ChevronLeftIcon className="h-7" />
+            </div>
+            <div className="p-2 cursor-pointer" ref={navigationNextRef}>
+              <ChevronRightIcon className="h-7" />
+            </div>
+          </div>
+        </Swiper>
       )}
     </section>
   );
