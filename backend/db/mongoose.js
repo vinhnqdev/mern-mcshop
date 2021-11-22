7584c1d3337eb84513f2465400e21160849a125d
Vinh Ngo <ngoquangvinh1997tg@gmail.com>
2021-11-22 21:29:08 +0700

    edit mongoose file

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -5,7 +5,7 @@ const connectBD = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      // useCreateIndex: true,
+      useCreateIndex: true,
     });
     console.log(chalk.blue.underline.bold(`MongoDB Connected: ${conn.connection.host}`));
   } catch (error) {
